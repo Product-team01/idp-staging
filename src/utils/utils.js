0c732c0bbb3e1e918b7adef32e361cb2e1613980
silasashar1 <silas@madstreetden.com>
2024-08-09 15:10:01 +0530

    changes

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,9 @@
 export const getCookie = (key) => {
-    const searchKey = `${key}=`;
-    const cookies = document.cookie ? document.cookie.split('; ') : [];
-    const result = cookies.find((cookie) => cookie.startsWith(searchKey));
-    if (result) {
-      return result.split('=')[1];
-    }
-    return null;
-  };
-  
+  const searchKey = `${key}=`;
+  const cookies = document.cookie ? document.cookie.split('; ') : [];
+  const result = cookies.find((cookie) => cookie.startsWith(searchKey));
+  if (result) {
+    return result.split('=')[1];
+  }
+  return null;
+};
